31b9a7cda64fcf0d8d423a80c42b23e6bb0b3278
George Fane <gfane@umich.edu>
2021-09-01 03:03:54 +0000

    added gcloud function code

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Room } from '@material-ui/icons';
-import bootstrapURLKeys from './env';
 
 function Marker(props) {
     return (
